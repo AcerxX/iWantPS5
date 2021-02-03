49811251867479fbf55d7f54720b19814401f50f
Alexandru Mihai <alexandru.mihai@it-genetics.ro>
2021-02-03 15:59:37 +0200

    better

--- src/main/java/ro/softica/iWantPS5/EndlessWorker.java
+++ src/main/java/ro/softica/iWantPS5/EndlessWorker.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
 @Scope
@@ -22,33 +23,28 @@ public class EndlessWorker extends Thread {
     @SneakyThrows
     @Override
     public void run() {
-//        webhook.setContent("Alerta PS5 pornita!");
-//        webhook.setAvatarUrl("https://edsurge.imgix.net/uploads/post/image/10801/shutterstock_493677196-1519849569.jpg");
-//        webhook.setUsername("iWantPS5");
-//        webhook.setTts(false);
-//        try {
-//            webhook.execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         while (true) {
-            System.out.println("Searching...");
+            System.out.println(LocalDateTime.now() + " - Searching...");
 
-            testEmag();
-            detectPS5OnEmag();
+            try {
+                detectPS5OnEmag();
+                detectPS5OnOrange();
+                detectPS5OnAltex();
+                detectPS5OnMediaGalaxy();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
 
-            testOrange();
-            detectPS5OnOrange();
-
-            System.out.println("All good! Sleeping...");
-            sleep(30000);
+            System.out.println(LocalDateTime.now() + " - None found! Sleeping...");
+            sleep(60000);
         }
     }
 
     private void detectPS5OnEmag() throws IOException {
         String link = "https://www.emag.ro/consola-playstation-5-so-9396406/pd/DNKW72MBM/";
-        Document doc = Jsoup.connect(link).get();
+        Document doc = Jsoup.connect(link)
+                .userAgent("Opera")
+                .get();
         Elements addToCartButtons = doc.select(".yeahIWantThisProduct");
 
         if (!addToCartButtons.isEmpty()) {
@@ -57,6 +53,74 @@ public class EndlessWorker extends Thread {
             yellPS5Found(link, doc, "@everyone PS5 GASIT PE EMAG!!!", "PS5 gasit pe EMAG");
         }
     }
+
+    private void detectPS5OnOrange() throws IOException {
+        String link = "https://www.orange.ro/magazin-online/obiecte-conectate/consola-playstation-5";
+        Document doc = Jsoup.connect(link)
+                .userAgent("Opera")
+                .get();
+        Elements addToCartButtons = doc.select("#productprice");
+
+        if (!addToCartButtons.isEmpty()) {
+            yellPS5Found(link, doc, "@everyone PS5 GASIT PE ORANGE!!!", "PS5 gasit pe ORANGE");
+            yellPS5Found(link, doc, "@everyone PS5 GASIT PE ORANGE!!!", "PS5 gasit pe ORANGE");
+            yellPS5Found(link, doc, "@everyone PS5 GASIT PE ORANGE!!!", "PS5 gasit pe ORANGE");
+        }
+    }
+
+    private void detectPS5OnAltex() throws IOException {
+        String link = "https://altex.ro/console-ps5/cpl/";
+        Document doc = Jsoup.connect(link)
+                .userAgent("Opera")
+                .get();;
+        Elements addToCartButtons = doc.select("div.text-alertYellow");
+
+        if (addToCartButtons.isEmpty()) {
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE ALTEX!!!", "PS5 gasit pe ALTEX");
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE ALTEX!!!", "PS5 gasit pe ALTEX");
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE ALTEX!!!", "PS5 gasit pe ALTEX");
+        }
+    }
+
+    private void detectPS5OnMediaGalaxy() throws IOException {
+        String link = "https://mediagalaxy.ro/console-ps5/cpl/";
+        Document doc = Jsoup.connect(link)
+                .userAgent("Opera")
+                .get();;
+        Elements addToCartButtons = doc.select("div.text-alertYellow");
+
+        if (addToCartButtons.isEmpty()) {
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE MEDIA GALAXY!!!", "PS5 gasit pe MEDIA GALAXY");
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE MEDIA GALAXY!!!", "PS5 gasit pe MEDIA GALAXY");
+             yellPS5Found(link, doc, "@everyone PS5 GASIT PE MEDIA GALAXY!!!", "PS5 gasit pe MEDIA GALAXY");
+        }
+    }
+
+    private void yellPS5Found(String link, Document doc, String s, String s2) throws IOException {
+        webhook.setContent(s);
+        webhook.setAvatarUrl("https://s13emagst.akamaized.net/products/32502/32501277/images/res_8964c0a2056c6f1fc96531e6f5721fb2.jpg");
+        webhook.setUsername("iWantPS5");
+        webhook.setTts(false);
+        webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                .setTitle(s2)
+                .setDescription(link)
+                .setColor(Color.RED)
+                .addField("Produs", doc.title(), false)
+                .addField("Link", link, false)
+                .setImage("https://s13emagst.akamaized.net/products/32502/32501277/images/res_8964c0a2056c6f1fc96531e6f5721fb2.jpg")
+                .setUrl(link));
+        webhook.execute();
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+        System.out.println(s2);
+    }
+
 
     private void testEmag() throws IOException {
         String link = "https://www.emag.ro/telefon-mobil-samsung-galaxy-s21-dual-sim-128gb-8gb-ram-5g-phantom-grey-sm-g991bzadeue/pd/DPLGTDMBM/";
@@ -84,31 +148,5 @@ public class EndlessWorker extends Thread {
             webhook.setTts(false);
             webhook.execute();
         }
-    }
-
-    private void detectPS5OnOrange() throws IOException {
-        String link = "https://www.orange.ro/magazin-online/obiecte-conectate/consola-playstation-5";
-        Document doc = Jsoup.connect(link).get();
-        Elements addToCartButtons = doc.select("#productprice");
-
-        if (!addToCartButtons.isEmpty()) {
-            yellPS5Found(link, doc, "@everyone PS5 GASIT PE ORANGE!!!", "PS5 gasit pe ORANGE");
-        }
-    }
-
-    private void yellPS5Found(String link, Document doc, String s, String s2) throws IOException {
-        webhook.setContent(s);
-        webhook.setAvatarUrl("https://s13emagst.akamaized.net/products/32502/32501277/images/res_8964c0a2056c6f1fc96531e6f5721fb2.jpg");
-        webhook.setUsername("iWantPS5");
-        webhook.setTts(false);
-        webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                .setTitle(s2)
-                .setDescription(link)
-                .setColor(Color.RED)
-                .addField("Produs", doc.title(), false)
-                .addField("Link", link, false)
-                .setImage("https://s13emagst.akamaized.net/products/32502/32501277/images/res_8964c0a2056c6f1fc96531e6f5721fb2.jpg")
-                .setUrl(link));
-        webhook.execute();
     }
 }
